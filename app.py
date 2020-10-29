0164f793fd35fedfe1f51fd1c385e11c4fd2aa2d
Cerulean Canvas <42316559+cerulean-canvas@users.noreply.github.com>
2020-10-29 19:52:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+  
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello():
+    return "Are you enjoying this Class? Don't forget to rate us!!😊😊 "
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
